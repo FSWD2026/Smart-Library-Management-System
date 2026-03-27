653ac7ad7b7c327c4fc167b71105e764f54af228
Manthan-1795 <koremanthanp@gmail.com>
2026-03-27 11:14:25 +0530

    connect backend and added admin api's

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
+const adminRouter = require("./routes/admin");
+
 const app = express();
 const PORT = 4000;
 
+app.use("/admin", adminRouter);
 
 app.listen(PORT, "localhost", () => {
   console.log(`Server Started At Port ${PORT}`);
